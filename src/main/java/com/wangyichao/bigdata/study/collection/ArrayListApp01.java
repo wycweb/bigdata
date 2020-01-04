2053510d92282f4fe1687778f0e760eac0848afc
wangyichao <xiaochao19960326>
2020-01-05 00:32:09 +0800

    day03:java ArrayList源码复习

---
+++ src/main/java/com/wangyichao/bigdata/study/collection/ArrayListApp01.java
@@ -0,0 +1,20 @@
+package com.wangyichao.bigdata.study.collection;
+
+public class ArrayListApp01 {
+
+    /**
+     * List接口的主要实现类
+     * 线程不安全、效率高，底层使用Object[] element
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * ArrayList源码分析
+     * jdk7和jdk8版本有一些不同
+     *
+     */
+}
