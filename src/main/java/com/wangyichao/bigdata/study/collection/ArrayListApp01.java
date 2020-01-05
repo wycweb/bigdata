54f3f2604b15591f110a4706ae5837ff3107664c
wangyichao <xiaochao19960326>
2020-01-06 00:25:33 +0800

    day04:java Collection相关代码源码分析学习

--- src/main/java/com/wangyichao/bigdata/study/collection/ArrayListApp01.java
+++ src/main/java/com/wangyichao/bigdata/study/collection/ArrayListApp01.java
@@ -4,17 +4,11 @@ public class ArrayListApp01 {
 
     /**
      * List接口的主要实现类
-     * 线程不安全、效率高，底层使用Object[] element
+     * 线程不安全、效率高，底层使用Object[] elementData存储
+     * 笔记地址：https://blog.csdn.net/weixin_44641024/article/details/103839251
      * @param args
      */
     public static void main(String[] args) {
 
     }
-
-
-    /**
-     * ArrayList源码分析
-     * jdk7和jdk8版本有一些不同
-     *
-     */
 }
