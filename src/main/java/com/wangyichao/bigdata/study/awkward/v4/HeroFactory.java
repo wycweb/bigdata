7dc1c1acd6b68425b68dbe0f7092d2436aea76d7
wangyichao <xiaochao19960326>
2020-01-17 01:22:57 +0800

    day10:java IOC DI学习理解

--- src/main/java/com/wangyichao/bigdata/study/awkward/v4/HeroFactory.java
+++ src/main/java/com/wangyichao/bigdata/study/awkward/v4/HeroFactory.java
@@ -2,7 +2,11 @@ package com.wangyichao.bigdata.study.awkward.v4;
 
 public class HeroFactory {
 
-    //通过反射机制，完成输入名字直接实例化对象
+    /**
+     * 通过反射机制，完成输入名字直接实例化对象
+     * 缺陷：目前每次调用都要反射，这样性能会偏低
+     * 工厂方法 + 反射 并不是  IOC + DI
+     */
     public static SkillImpl getHero(String name) throws Exception {
         /**
          * 反射
